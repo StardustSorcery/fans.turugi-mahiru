9fd31268a070da39aea2862d404cb8c01e5284fa
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2023-10-28 21:50:40 +0900

    #4 MD2Material 実装 (www.ha-rehare.fans から移植) / プライバシーポリシーページ実装

---
+++ services/www/src/app/(default)/(common)/privacy/page.tsx
@@ -0,0 +1,93 @@
+import {
+  Box,
+  Container,
+  Paper,
+  Typography,
+} from "@mui/material";
+import Heading1 from "@/components/Heading/Heading1";
+import { StrapiResponse } from "strapi-sdk-js";
+import { Privacy, StrapiResponseData } from "@/types/strapi";
+import date2str from "@/utils/date2str";
+import MD2Material from "@/components/MD2Material/MD2Material";
+
+function fetchPrivacy(): Promise<{
+  data: StrapiResponse<StrapiResponseData<Privacy>> | null;
+  error: Error | null;
+}> {
+  const url = `http://localhost:${process.env.PORT || '80'}/api/privacy`;
+
+  return fetch(url, { cache: 'no-cache' })
+    .then(resp => (resp.json() as unknown) as StrapiResponse<StrapiResponseData<Privacy>>)
+    .then(data => ({
+      data,
+      error: null,
+    }))
+    .catch((err: Error) => {
+      console.error(err);
+      return {
+        data: null,
+        error: err,
+      };
+    });
+}
+
+export default async function PrivacyPage() {
+  const {
+    data: privacy,
+    error: privacyError,
+  } = await fetchPrivacy();
+
+  return (
+    <>
+      <Box
+        component="main"
+        my={4}
+      >
+        <Container
+          maxWidth="lg"
+        >
+          <Box
+            component={Paper}
+            elevation={6}
+          >
+            <Heading1
+              text="プライバシーポリシー"
+            />
+
+            {privacyError || !privacy ? (
+              <Typography
+                align="center"
+                component="p"
+                variant="subtitle1"
+              >
+                読み込みに失敗しました.
+              </Typography>
+            ) : (
+              <Box
+                px={2}
+                py={3}
+              >
+                <MD2Material
+                  markdown={privacy.data.attributes.body}
+                />
+
+                <Typography
+                  align="right"
+                >
+                  以上
+                </Typography>
+                <Typography
+                  component="p"
+                  variant="subtitle2"
+                  mt={4}
+                >
+                  {date2str(new Date(privacy.data.attributes.updatedAt))} 最終更新・発効
+                </Typography>
+              </Box>
+            )}
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
+}
