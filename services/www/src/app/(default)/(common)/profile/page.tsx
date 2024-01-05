140071d08ed7511ee3ce1b3447bbb1c3b72fbafc
Yuta NAKAMIZO <yuta.nakamizo@ggtk.app>
2024-01-06 04:24:45 +0900

    #14 プロフィールページ作成 (#23)

--- services/www/src/app/(default)/(common)/profile/page.tsx
+++ services/www/src/app/(default)/(common)/profile/page.tsx
@@ -1,3 +1,10 @@
+import {
+  Box,
+  Container,
+  Paper,
+} from "@mui/material";
+import Profile from "./_components/Profile";
+
 export const metadata = {
   title: 'プロフィール | 剣城まひる.fans - 非公式ファンサイト',
   description: 'VTuber『剣城 (つるぎ) まひる』さんの非公式ファンサイト',
@@ -6,6 +13,27 @@ export const metadata = {
 export default function ProfilePage() {
   return (
     <>
+      <Box
+        component="main"
+        my={4}
+      >
+        <Container
+          maxWidth="lg"
+        >
+          <Box
+            component={Paper}
+            elevation={6}
+          >
+            <Box
+              px={2}
+              py={3}
+            >
+              <Profile
+              />
+            </Box>
+          </Box>
+        </Container>
+      </Box>
     </>
   );
 }
