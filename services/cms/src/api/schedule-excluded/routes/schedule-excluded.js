6180a3efdef1a508c9410caf37656a6282705283
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2024-01-07 12:19:10 +0900

    #24 cms ScheduleExcluded Single Type を追加

---
+++ services/cms/src/api/schedule-excluded/routes/schedule-excluded.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * schedule-excluded router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::schedule-excluded.schedule-excluded');
