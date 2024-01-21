5247bcaf6b0ed25b73f2666c3ac6626873c03e00
Yuta Nakamizo <yuta.nakamizo@ggtk.app>
2024-01-22 02:54:29 +0900

    #9 cms RankingUserVoting と Ranking モデルを追加

---
+++ services/cms/src/api/ranking-user-voting/controllers/ranking-user-voting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ranking-user-voting controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::ranking-user-voting.ranking-user-voting');
---
+++ services/cms/src/api/ranking-user-voting/services/ranking-user-voting.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * ranking-user-voting service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::ranking-user-voting.ranking-user-voting');
