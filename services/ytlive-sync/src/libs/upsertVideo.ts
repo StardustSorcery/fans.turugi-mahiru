e24e37ae34c89e858c1d6f79a524e10cae8f1136
Yuta NAKAMIZO <yuta.nakamizo@ggtk.app>
2024-01-26 01:40:18 +0900

    #64 www ytlive-sync の upsertVideo で redis 接続を最後に解除 (#65)

--- services/ytlive-sync/src/libs/upsertVideo.ts
+++ services/ytlive-sync/src/libs/upsertVideo.ts
@@ -9,7 +9,7 @@ const logger = log4js.init().getLogger();
 export default async function upsertVideo(record: Omit<Video, 'etag' | 'raw'>, raw: any, recordId?: number) {
   const redisClient = await redis.init();
 
-   await redis.connect(redisClient);
+  await redis.connect(redisClient);
 
   // check Etag (MD5 hash)
   const etag = objectHash.MD5(record);
@@ -56,60 +56,62 @@ export default async function upsertVideo(record: Omit<Video, 'etag' | 'raw'>, r
         throw err;
       });
 
-    // create or update record
-    const fullRecord: Video = {
-      ...record,
-      etag,
-      raw,
-    };
+  // create or update record
+  const fullRecord: Video = {
+    ...record,
+    etag,
+    raw,
+  };
 
-    if(entryId === null) {
-      await strapi
-        .create<StrapiResponseData<Video>>(
-          'videos',
-          fullRecord,
-          {
-            fields: [],
-          }
-        )
-        .then(result => {
-          logger.debug(`[Strapi.create<Video>] Created video entry (id: ${result.data.id}).`);
-        })
-        .catch(err => {
-          err.message = `[Strapi.create<Video>] ${err.message}`;
-          throw err;
-        });
-    }
-    else {
-      await strapi
-        .update<StrapiResponseData<Video>>(
-          'videos',
-          entryId,
-          fullRecord,
-          {
-            fields: [],
-          }
-        )
-        .then(() => {
-          logger.debug(`[Strapi.update<Video>] Updated video entry (id: ${entryId}).`)
-        })
-        .catch(err => {
-          err.message = `[Strapi.update<Video>] ${err.message}`;
-          throw err;
-        });
-    }
-
-    // store etag
-    await redisClient
-      .hSet(redis.keys.videoEtagHash, `youtube:${record.videoId}`, etag)
-      .then(() => {
-        logger.debug(`[Redis.HSET(videoEtagHash)] Stored etag of the video "${record.title}" (${record.videoId}).`);
-        return;
+  if(entryId === null) {
+    await strapi
+      .create<StrapiResponseData<Video>>(
+        'videos',
+        fullRecord,
+        {
+          fields: [],
+        }
+      )
+      .then(result => {
+        logger.debug(`[Strapi.create<Video>] Created video entry (id: ${result.data.id}).`);
       })
       .catch(err => {
-        err.message = `[Redis.HSET(videoEtagHash)] ${err.message}`;
+        err.message = `[Strapi.create<Video>] ${err.message}`;
         throw err;
       });
+  }
+  else {
+    await strapi
+      .update<StrapiResponseData<Video>>(
+        'videos',
+        entryId,
+        fullRecord,
+        {
+          fields: [],
+        }
+      )
+      .then(() => {
+        logger.debug(`[Strapi.update<Video>] Updated video entry (id: ${entryId}).`)
+      })
+      .catch(err => {
+        err.message = `[Strapi.update<Video>] ${err.message}`;
+        throw err;
+      });
+  }
+
+  // store etag
+  await redisClient
+    .hSet(redis.keys.videoEtagHash, `youtube:${record.videoId}`, etag)
+    .then(() => {
+      logger.debug(`[Redis.HSET(videoEtagHash)] Stored etag of the video "${record.title}" (${record.videoId}).`);
+      return;
+    })
+    .catch(err => {
+      err.message = `[Redis.HSET(videoEtagHash)] ${err.message}`;
+      throw err;
+    });
+
+  await redisClient.disconnect();
 
   return;
 }
